3d72bbba548eba6e79317b8246e8bf69f6d32559
Johan JNN <johan.janin@gmail.com>
2026-05-20 10:32:00 +0200

    feat: header & button components finished (#9)

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,36 +1,49 @@
 <script lang="ts" module>
+	import type { ComponentProps } from 'svelte';
+
 	export const NAVIGATION = {
 		Accueil: '/',
 		Offres: '/jobs',
 		Entreprises: '/companies',
 		'A propos': '/fyn',
-		Contact: '/contact'
+		Contact: '/contact',
+		Test: '/header'
 	};
-	export const NAVIGATION_CTAS = {
-		'Se connecter': '/login',
-		'Je recrute': 'https://hire.fyn.com'
+	export const NAVIGATION_CTAS: {
+		[key: string]: { url: string; type: ComponentProps<typeof Button>['type'] };
+	} = {
+		'Se connecter': { url: '/login', type: 'primary' },
+		'Je recrute': { url: 'https://hire.fyn.com', type: 'secondary' }
 	};
 </script>
 
 <script lang="ts">
 	import { page } from '$app/state';
-
 	import Logo from '$lib/assets/brand/logo.png';
+	import Button from './ui/button.svelte';
 </script>
 
-<header>
-	<a href="/">
+<header class="fixed top-0 left-0 flex w-full items-center justify-between px-8 py-4">
+	<a href="/" class="flex h-12 w-fit">
 		<img src={Logo} alt="Logo Fyn" />
 	</a>
-	<nav>
-		<ul>
+	<nav class="rounded-full bg-reflect-yellow p-1.5 font-medium text-ocean-blue">
+		<ul class="flex h-full items-center justify-between gap-0.5">
 			{#each Object.entries(NAVIGATION) as [label, url_str]}
 				{@const url = new URL(url_str, page.url)}
-				{@const is_local = url.host !== page.url.host}
-				{@const is_current = is_local && page.url.pathname === page.url.pathname}
+				{@const is_local = url.host === page.url.host}
+				{@const is_current = is_local && url.pathname === page.url.pathname}
 
 				<li>
-					<a aria-hidden={is_current} href={url.toString()} target={is_local ? '_self' : '_blank'}>
+					<a
+						class={{
+							'px-4 py-2': true,
+							'block rounded-full bg-white font-semibold': is_current
+						}}
+						aria-hidden={is_current}
+						href={url.toString()}
+						target={is_local ? '_self' : '_blank'}
+					>
 						{label}
 					</a>
 				</li>
@@ -39,15 +52,21 @@
 	</nav>
 
 	<nav>
-		<ul>
-			{#each Object.entries(NAVIGATION_CTAS) as [label, url_str]}
+		<ul class="flex items-center gap-2">
+			{#each Object.entries(NAVIGATION_CTAS) as [label, { url: url_str, type }]}
 				{@const url = new URL(url_str, page.url)}
 				{@const is_local = url.host !== page.url.host}
 
 				<li>
-					<a href={url.toString()} target={is_local ? '_self' : '_blank'}>
+					<Button
+						action={{
+							url,
+							target: is_local ? '_self' : '_blank'
+						}}
+						{type}
+					>
 						{label}
-					</a>
+					</Button>
 				</li>
 			{/each}
 		</ul>
