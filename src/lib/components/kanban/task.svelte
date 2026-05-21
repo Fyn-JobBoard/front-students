31eccf5c6cec869768437b6f797bbc34d4b20505
Johan JNN <johan.janin@gmail.com>
2026-05-21 12:41:38 +0200

    feat: kanban component & section

---
+++ src/lib/components/kanban/task.svelte
@@ -0,0 +1,22 @@
+<script module lang="ts">
+	export interface TaskProps {
+		title: string;
+		description: string;
+		themeColor: string;
+	}
+</script>
+
+<script lang="ts">
+	const { title, description, themeColor }: TaskProps = $props();
+</script>
+
+<article
+	style:--theme={themeColor}
+	class={[
+		'grid items-start rounded-lg border-2 border-bleuet-blue bg-bleuet-blue/30 p-4',
+		'before:inline-block before:aspect-[10] before:h-1 before:rounded-full before:bg-(--theme) before:content-[""]'
+	]}
+>
+	<h4 class="my-1">{title}</h4>
+	<p class="font-light text-ecume-blue">{description}</p>
+</article>
