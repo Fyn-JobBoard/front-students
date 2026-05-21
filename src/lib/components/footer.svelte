ee548b8bd9006150d6496abc55b8b547fc076088
Terence MAYOMBO <terencemayombo@MacBook-Pro-de-Terence.local>
2026-05-21 10:45:32 +0200

    fix/formatting svelte

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -53,23 +53,22 @@
 </script>
 
 <footer class="bg-blue-800 text-[#8C99C7]">
-	<div
-		class="mx-auto grid max-w-7xl grid-cols-1 gap-12 px-6 py-14 md:grid-cols-[230px_220px_1fr]"
->
+	<div class="mx-auto grid max-w-7xl grid-cols-1 gap-12 px-6 py-14 md:grid-cols-[230px_220px_1fr]">
+		<div>
+			<img src={Logo} alt="Logo Fyn" class="h-auto w-24" />
+		</div>
+		<div>
+			<p class="max-w-sm text-sm leading-6 text-[#8C99C7]">
+				La plateforme qui connecte les étudiants ambitieux avec les meilleures entreprises.
+			</p>
+		</div>
 
-	<div>
-		<img src={Logo} alt="Logo Fyn" class="h-auto w-24" />	
-	</div>
-	<div>
-		<p class="max-w-sm text-sm leading-6 text-[#8C99C7]">
-			La plateforme qui connecte les étudiants ambitieux avec les meilleures entreprises.</p>
-	</div>
-
-	<div class="grid grid-cols-1 gap-12 sm:grid-cols-2 md:grid-cols-3">
+		<div class="grid grid-cols-1 gap-12 sm:grid-cols-2 md:grid-cols-3">
 			{#each Object.entries(NAVIGATIONS) as [section, links]}
 				<div>
-					<h2 class="mb-5 text-xs font-bold uppercase tracking-widest text-white">
-						{section}</h2>
+					<h2 class="mb-5 text-xs font-bold tracking-widest text-white uppercase">
+						{section}
+					</h2>
 
 					<nav>
 						<ul class="space-y-2">
@@ -91,24 +90,29 @@
 	</div>
 
 	<div class="class mx-auto w-[85%] border-t border-white/10">
+		<div
+			class="mx-auto flex max-w-7xl flex-col items-center justify-between gap-5 px-3 py-18 md:flex-row"
+		>
+			<p class="text-sm text-[#8C99C7]">
+				© 2026 FYN - Tous droits réservés. Fait avec ❤ pour les alternants de France.
+			</p>
 
-	<div class="mx-auto flex max-w-7xl flex-col items-center justify-between gap-5 px-3 py-18 md:flex-row">
-		<p class="text-sm text-[#8C99C7]">
-			© 2026 FYN - Tous droits réservés. Fait avec ❤ pour les alternants de France.
-		</p>
-
-		<nav>
-			<ul class="flex items-center gap-6">
-				{#each Object.entries(SOCIALS) as [network, { url, icon }]}
-					<li>
-						<a href={url.toString()} target="_blank" title="Retrouve-nous sur {network} !"
-						class="flex h-10 w-10 items-center justify-center rounded-full border border-[#8C99C7]/30 text-xl text-[#8C99C7]">
-							<span class={[icon]}></span>
-						</a>
-					</li>
-				{/each}
-			</ul>
-		</nav>
-	</div>
+			<nav>
+				<ul class="flex items-center gap-6">
+					{#each Object.entries(SOCIALS) as [network, { url, icon }]}
+						<li>
+							<a
+								href={url.toString()}
+								target="_blank"
+								title="Retrouve-nous sur {network} !"
+								class="flex h-10 w-10 items-center justify-center rounded-full border border-[#8C99C7]/30 text-xl text-[#8C99C7]"
+							>
+								<span class={[icon]}></span>
+							</a>
+						</li>
+					{/each}
+				</ul>
+			</nav>
+		</div>
 	</div>
 </footer>
