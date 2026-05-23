dca148929fd4cf209925af7aa87268ba0f76f5f9
Johan JNN <johan.janin@gmail.com>
2026-05-23 16:43:07 +0200

    feat: metrics component (#2)

---
+++ src/lib/components/metrics/stats-bar.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { ComponentProps } from 'svelte';
+	import Metric from './metric.svelte';
+
+	const {
+		metrics
+	}: {
+		metrics: ComponentProps<typeof Metric>[];
+	} = $props();
+</script>
+
+<ul class="flex items-center justify-between rounded-3xl bg-white px-12 py-8">
+	{#each metrics as metric, index}
+		{#if index}
+			<li aria-hidden="true" class="flex h-full items-center justify-center">
+				<hr class="h-70/100 w-0.5 rounded-full border-none bg-bleuet-blue" />
+			</li>
+		{/if}
+		<li>
+			<Metric {...metric} />
+		</li>
+	{/each}
+</ul>
