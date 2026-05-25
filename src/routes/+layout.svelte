275b1fe14564c0c6f3115375fcfbb367914a2dcc
Johan Jnn <168817280+johan-jnn@users.noreply.github.com>
2026-05-25 18:01:51 +0200

    App base layout (#32)
    * feat: app base layout
    
    * fix: reduced the min height of the pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/brand/icon-yellow.svg';
+	import Footer from '$lib/components/footer.svelte';
+	import Header from '$lib/components/header.svelte';
 	import '$lib/styles/app.css';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<Header />
+
+<main class="min-h-[60svh]">
+	{@render children()}
+</main>
+
+<Footer />
