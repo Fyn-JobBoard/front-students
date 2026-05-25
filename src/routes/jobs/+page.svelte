b673310b0ead74e838b1739ead0d1943ee82b72a
Johan JNN <johan.janin@gmail.com>
2026-05-25 17:38:30 +0200

    feat: job page structure (#18)

--- src/routes/jobs/+page.svelte
+++ src/routes/jobs/+page.svelte
@@ -1,1 +1,11 @@
-<h1>Offres d'emploi</h1>
+<script>
+	import Hero from './hero.svelte';
+	import Navigator from './navigator.svelte';
+</script>
+
+<header>
+	<Hero />
+</header>
+<main class="mx-auto max-w-400 px-4">
+	<Navigator />
+</main>
