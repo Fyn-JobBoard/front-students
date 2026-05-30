9855c83ed136e766054d4aeb3cf288d6af3f7102
Johan JNN <johan.janin@gmail.com>
2026-05-30 12:52:06 +0200

    fix: hero responsive issues (#2)

--- src/routes/(homepage)/hero.svelte
+++ src/routes/(homepage)/hero.svelte
@@ -16,17 +16,17 @@
 </script>
 
 <script>
+	import Search from '$lib/components/forms/search.svelte';
 	import Simple from '$lib/components/heros/simple.svelte';
 	import StatsBar from '$lib/components/metrics/stats-bar.svelte';
-	import Search from '$lib/components/forms/search.svelte';
 </script>
 
 <Simple
 	title={['Trouve ton ', 'alternance', ', lance ta carrière']}
 	tag="La plateform #1 des alternants"
-	class="h-screen pt-32"
+	class="min-h-75svh pt-32"
 >
-	<div class="w-full grid gap-14">
+	<div class="grid w-full gap-14">
 		<Search
 			action="/jobs"
 			name="query"
