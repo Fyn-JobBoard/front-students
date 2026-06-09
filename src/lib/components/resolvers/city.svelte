516ba9408e5b6f299034b964570a0f915bfdbc51
Johan JNN <johan.janin@gmail.com>
2026-06-09 11:50:12 +0200

    feat: city resolver from lat/lng

---
+++ src/lib/components/resolvers/city.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { page } from '$app/state';
+
+	const {
+		lat,
+		lng
+	}: {
+		lat: number;
+		lng: number;
+	} = $props();
+
+	const params = $derived(
+		new URLSearchParams({
+			lat: lat.toString(),
+			lng: lng.toString()
+		})
+	);
+
+	const url = $derived(new URL(`/api/resolvers/city?${params.toString()}`, page.url));
+</script>
+
+{#await fetch(url).then((r) => r.json())}
+	{lat}/{lng}
+{:then { city }}
+	{city}
+{/await}
