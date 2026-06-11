4eb183cd0065b83cf900d685b81be5e54a313a03
Terence MAYOMBO <terencemayombo@MacBook-Pro-de-Terence.local>
2026-06-11 16:04:06 +0200

    finish page contact connect in formspree

--- src/routes/contact/contact.svelte
+++ src/routes/contact/contact.svelte
@@ -1,4 +1,4 @@
-<section class="bg-yellow py p-18 dark:bg-gray-900">
+<section class="bg-yellow py-18 dark:bg-gray-900">
     <div class="container px-6 py-12 mx-auto">
         <div>
             <p class="font-medium text-blue-500 dark:text-blue-400">Contactez-nous</p>
@@ -23,28 +23,31 @@
                 </div>
             </div>
 
-            <div class="p-4 py-6 rounded-lg bg-gray-50 dark:bg-gray-800 md:p-8">
-                <form>
+            <div >
+                <form
+                action="https://formspree.io/f/xbdeyqpq"
+                    method="POST"
+                 class="rounded-3xl border border-ocean-blue/10 bg-white/70 p-5 shadow-sm sm:p-8">
                     <div class="-mx-2 md:items-center md:flex">
                         <div class="flex-1 px-2">
-                            <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Prénom</label>
-                            <input type="text" placeholder="John " class="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-lighthouse-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                            <label for="first-name" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Prénom</label>
+                            <input id="first-name" name="Prénom" type="text" placeholder="Mike " class="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-lighthouse-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                         </div>
 
                         <div class="flex-1 px-2 mt-4 md:mt-0">
-                            <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Nom</label>
-                            <input type="text" placeholder="Doe" class="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-lighthouse-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                            <label for="last-name" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Nom</label>
+                            <input id="last-name" name="Nom" type="text" placeholder="Mirou" class="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-lighthouse-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-40  focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                         </div>
                     </div>
 
                     <div class="mt-4">
-                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Adresse mail</label>
-                        <input type="email" placeholder="johndoe@example.com" class="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-lighthouse-white border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                        <label for="email" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Adresse mail</label>
+                        <input id="email" name="email" type="email" placeholder="mikemirou@gmail.com" class="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-lighthouse-white border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                     </div>
 
                     <div class="w-full mt-4">
-                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Message</label>
-                        <textarea class="block w-full h-32 px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-lighthouse-white border-gray-200 rounded-lg md:h-56 dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Message"></textarea>
+                        <label for="message" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Message</label>
+                        <textarea id="message" name="message" class="block w-full h-32 px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-lighthouse-white border-gray-200 rounded-lg md:h-56 dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Message"></textarea>
                     </div>
 
                     <button class="w-full px-6 py-3 mt-4 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
