876766b78e8a052898527bd613721f8465fd9767
berlus10 <djongonberlus@gmail.com>
2026-06-19 03:49:39 +0100

    feat: section offres similaires #59

--- src/routes/jobs/[id]/+page.server.ts
+++ src/routes/jobs/[id]/+page.server.ts
@@ -3,7 +3,6 @@ import { FynFetchClients, useApi } from '$lib/server/api/api';
 import { JobsApi, type Job } from 'fyn-api-sdk';
 import type { PageServerLoad } from './$types';
 
-
 export const load: PageServerLoad = async ({ cookies, fetch, params }) => {
 	const jobs = useApi(
 		JobsApi,
@@ -15,7 +14,16 @@ export const load: PageServerLoad = async ({ cookies, fetch, params }) => {
 		throw error(404, 'Offre introuvable');
 	});
 
+	const similarJobs = await jobs
+		.jobsControllerFindAllV1(1, undefined, job.activity_domain?.name)
+		.then((r) => r.list.filter((j: Job) => j.id !== job.id).slice(0, 3))
+		.catch((reason) => {
+			console.error(reason);
+			return [] as Job[];
+		});
+
 	return {
-		job
+		job,
+		similarJobs
 	};
 };
