f9e8301a17488538469ce08df76b53567086f619
berlus10 <djongonberlus@gmail.com>
2026-06-19 17:30:51 +0100

    Update src/lib/components/jobs/similar-offers.svelte
    Co-authored-by: Johan Jnn <168817280+johan-jnn@users.noreply.github.com>

--- src/lib/components/jobs/similar-offers.svelte
+++ src/lib/components/jobs/similar-offers.svelte
@@ -6,7 +6,7 @@
 </script>
 
 {#if jobs.length > 0}
-	<section class="mx-auto max-w-7xl px-4 py-14 sm:px-6">
+	<section class="mx-auto max-w-7xl px-4 py-14 sm:px-6 mt-8">
 		<h2 class="mb-8 font-headings text-2xl font-extrabold text-ocean-blue">Offres similaires</h2>
 		<ul class="grid grid-cols-3 gap-5 max-lg:grid-cols-2 max-sm:grid-cols-1">
 			{#each jobs as job}
